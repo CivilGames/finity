ca81dbd2031b7632ae14dd2a08408877f8308c0e
Kevin Bowey <kevin.bowey@athlon.com>
2023-09-18 15:09:25 +0200

    using a tilemap.. changed the size to 16x16

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
-@onready var horizontal = $Graphics/CanvasLayer/horizontal
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("ui_down"):
-		print('down')
-		print(horizontal.visible)
-		horizontal.visible = !horizontal.visible
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	for i in range(100):
+		print(dice(rng,4))
+
+
+func dice(rng: RandomNumberGenerator, eyes: int) -> int:
+	return rng.randi_range(1,eyes)
+
+
+# Zone
+zone = {
+	''
+}
