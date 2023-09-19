29dffbd38839b74dbadbd648e3edc69f6f88f5ea
Kevin Bowey <kevin.bowey@athlon.com>
2023-09-19 02:16:09 +0200

    ready to implement some actual gameplay.. dont get bogget bogged down with things like font (tried to create a custom font)

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -1,16 +1,27 @@
 extends Node2D
 
+@onready var _animated_cursor = $CursorAnimation
+@onready var full_menu = $HUD/FullMenu
 var current_position: Array = [0,0]
 
+var number_font = BitMapFont.new()
+
+
+func _ready():
+	_animated_cursor.play()
+
 func _input(event):
-	if event.is_action_pressed("ui_right"):
+	if event.is_action_pressed("right"):
 		current_position[0] += 16
-	if event.is_action_pressed("ui_left"):
+	if event.is_action_pressed("left"):
 		current_position[0] -= 16
-	if event.is_action_pressed("ui_up"):
+	if event.is_action_pressed("up"):
 		current_position[1] -= 16
-	if event.is_action_pressed("ui_down"):
+	if event.is_action_pressed("down"):
 		current_position[1] += 16
+		
+	if event.is_action_pressed("B"):
+		full_menu.visible = !full_menu.visible
 	
 		
 	position = Vector2(current_position[0], current_position[1])
