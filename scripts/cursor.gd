f8e6440ca417b99e171ff5a0da55642edb334ba6
Kevin Bowey <kevin.bowey@athlon.com>
2023-09-20 17:42:53 +0200

    refactoring project set-up

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 @onready var _animated_cursor = $CursorAnimation
-@onready var full_menu = $HUD/FullMenu
+@onready var full_menu = %FullMenu
+@onready var  camera =  %HUDCamera
 var current_position: Array = [0,0]
 
 
 func _ready():
 	_animated_cursor.play()
+	print('hi')
+	print(range(10,100))
 
 func _input(event):
 	if event.is_action_pressed("right"):
@@ -21,5 +24,14 @@ func _input(event):
 	if event.is_action_pressed("B"):
 		full_menu.visible = !full_menu.visible
 	
+	if event.is_action_pressed("A"):
+		print(str('cam',camera.position))
+		print(str('cur', position))
+	
+		camera.position = position
+		print(str('new_cam_pos',camera.position))
+	
+#	if event.is_action_pressed
 		
 	position = Vector2(current_position[0], current_position[1])
+
