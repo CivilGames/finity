f30a82c7293eb2a1b6da283ed0cbb59f101f710f
Kevin Bowey <kevin.bowey@athlon.com>
2023-09-23 17:29:03 +0200

    generated universe data now available through singleton

--- scripts/ui/Cursor.gd
+++ scripts/ui/Cursor.gd
@@ -1,19 +1,14 @@
 extends Node2D
 
 @onready var sprite = $Sprite
-@onready var hud = get_node("HUD")
+#@onready var hud = get_node("HUD")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	sprite.play()
-	var hud = get_node("HUD")
+#	var hud = get_node("HUD")
 #	hud.mode.connect(_on_mode_received)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_mode_received(mode, toggled):
--- scripts/universe/astro_universe.gd
+++ scripts/universe/astro_universe.gd
@@ -3,11 +3,14 @@ extends Node2D
 
 @onready var galaxy_holder = $Galaxies
 @onready var galaxy_scene = load('res://scenes/universe/astro_galaxy.tscn')
+@onready var universe_data = get_node('/root/AstroGenerators')
 
-func _set_astro_galaxies():
-	for i in range(3):
+func _set_astro_galaxies(data):
+
+	for i in len(data['galaxies'][0]):
 		var galaxy = galaxy_scene.instantiate()
 		galaxy_holder.add_child(galaxy)
 
 func _ready():
-	_set_astro_galaxies()
+	var data = universe_data.universe
+	_set_astro_galaxies(data)
